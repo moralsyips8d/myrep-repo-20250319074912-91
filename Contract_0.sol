4929a5be48b188b8ae11b18fdb6d061edb22653f
moralsyips8d <moralsyips8d@icloud.com>
2025-03-19 07:49:22 +0200

    📝 Commit 0 with SOLIDITY file

--- Contract_0.sol
+++ Contract_0.sol
@@ -2,13 +2,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract HelloWorld_0 {
+contract SimpleStorage_0 {
     uint256 public randomNumber;
     string public message;
 
     constructor() {
-        randomNumber = 97;
-        message = "Hello from commit 0 - 2025-03-19 07:49:18";
+        randomNumber = 8;
+        message = "Hello from commit 0 - 2025-03-19 07:49:21";
     }
 
     function getMessage() public view returns (string) {
